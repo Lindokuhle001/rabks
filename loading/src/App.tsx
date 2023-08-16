d9cfa2e496e286dc94dac3e7828065686bae46d3
lindokuhle shabalala <mlixshaba@gmail.com>
2023-08-16 15:09:52 +0200

    loading animatuion

--- loading/src/App.tsx
+++ loading/src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(5);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(previousCount => {
+        if (previousCount < 100) {
+          return previousCount + 10
+        } else {
+          clearInterval(interval);
+          return previousCount
+        }
+      });
+
+     
+    },1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <>
- 
+      <div className="loading">
+        <div className="bar" style={{ width: `${count}%` }}></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
